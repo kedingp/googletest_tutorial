007284665837824a6f1c23eb13d34262c095205d
Keding Philipp (ITKG/ENG31.3) <Philipp.Keding2@de.bosch.com>
2020-03-24 09:42:39 +0100

    Add sample unit tests of the official googletest repo.

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -7,13 +7,13 @@ namespace
     {
         googletest_tutorial::Calculator calculator;
         int result = calculator.add(3, 4);
-        ASSERT_EQ(result, 0);
+        ASSERT_EQ(result, 7);
     }
 
     TEST(testCalculator, addDoubles)
     {
         googletest_tutorial::Calculator calculator;
         double result = calculator.add(3.1, 4.2);
-        ASSERT_EQ(result, 0.0);
+        ASSERT_DOUBLE_EQ(result, 7.3);
     }
 } // namespace
